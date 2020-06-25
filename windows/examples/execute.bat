729010485262a4ca629613a5658e16f15ed3091d
bright <liyuliang001@gmail.com>
2020-06-24 22:46:54 -0400

    delete useless files

--- windows/examples/execute.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-
-for /r %%i in (*.exe) do (
-	
-     	echo "Executing %%i" 
-        %%~ni.exe
-)
--- windows/ns-3-dev/headers/gsl_export.h
+++
@@ -1,4 +0,0 @@
-#ifndef _GSL_EXPORT_H_
-#define _GSL_EXPORT_H_
-double  gsl_sf_erfc(double x);
-#endif
