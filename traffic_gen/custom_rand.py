4e07dfdb0dd497992b2b0c450342d4c5d5a35965
bright <liyuliang001@gmail.com>
2020-07-15 18:22:11 -0400

    update custom_rand.py

--- traffic_gen/custom_rand.py
+++ traffic_gen/custom_rand.py
@@ -27,8 +27,30 @@ class CustomRand:
 		return s/100
 	def rand(self):
 		r = random.random() * 100
+		return self.getValueFromPercentile(r)
+	def getPercentileFromValue(self, x):
+		if x < 0 or x > self.cdf[-1][0]:
+			return -1
 		for i in range(1, len(self.cdf)):
-			if r <= self.cdf[i][1]:
+			if x <= self.cdf[i][0]:
+				x0, y0 = self.cdf[i-1]
+				x1, y1 = self.cdf[i]
+				return y0 + (y1-y0)/(x1-x0)*(x-x0)
+	def getValueFromPercentile(self, y):
+		for i in range(1, len(self.cdf)):
+			if y <= self.cdf[i][1]:
 				x0,y0 = self.cdf[i-1]
 				x1,y1 = self.cdf[i]
-				return x0 + (x1-x0)/(y1-y0)*(r-y0)
+				return x0 + (x1-x0)/(y1-y0)*(y-y0)
+	def getIntegralY(self, y):
+		s = 0
+		for i in range(1, len(self.cdf)):
+			x0, y0 = self.cdf[i-1]
+			x1, y1 = self.cdf[i]
+			if y <= self.cdf[i][1]:
+				s += 0.5 * (x0 + x0+(x1-x0)/(y1-y0)*(y-y0))*(y-y0) / 100.
+				break
+			else:
+				s += 0.5 * (x1 + x0) * (y1 - y0) / 100.
+
+		return s
