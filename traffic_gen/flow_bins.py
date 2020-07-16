14cf7a3cd4812b853ea3ce0d6bcc528e9cd5a9d0
bright <liyuliang001@gmail.com>
2020-07-15 20:27:42 -0400

    x

--- traffic_gen/flow_bins.py
+++ traffic_gen/flow_bins.py
@@ -5,30 +5,7 @@ import heapq
 from optparse import OptionParser
 from custom_rand import CustomRand
 
-class Flow:
-	def __init__(self, src, dst, size, t):
-		self.src, self.dst, self.size, self.t = src, dst, size, t
-	def __str__(self):
-		return "%d %d 3 100 %d %.9f"%(self.src, self.dst, self.size, self.t)
-
-def translate_bandwidth(b):
-	if b == None:
-		return None
-	if type(b)!=str:
-		return None
-	if b[-1] == 'G':
-		return float(b[:-1])*1e9
-	if b[-1] == 'M':
-		return float(b[:-1])*1e6
-	if b[-1] == 'K':
-		return float(b[:-1])*1e3
-	return float(b)
-
-def poisson(lam):
-	return -math.log(1-random.random())*lam
-
 if __name__ == "__main__":
-	port = 80
 	parser = OptionParser()
 	parser.add_option("-c", "--cdf", dest = "cdf_file", help = "the file of the traffic size cdf", default = "uniform_distribution.txt")
 	options,args = parser.parse_args()
